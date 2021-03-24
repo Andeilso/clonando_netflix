07f1253a3c42ff4476904785b7b4968c98ad7968
Andeilso <andeilsoalves@gmail.com>
2021-03-24 14:24:29 -0300

    Carrosel de animes adicionado, menu lateral adicionado, mudanças estéticas no filme-destaque, navegador da página modificado para 'menu lateral'

---
+++ js/owl/setup.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    autoHeigght: true,
+    responsive: {
+        0:{
+            items:2
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:7
+        }
+    }
+})
